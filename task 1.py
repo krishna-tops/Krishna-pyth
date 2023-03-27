037fc06add82b6f9cd68189466342b3a5b9efd1c
unknown <kishu7892@gmail.com>
2023-03-28 02:30:25 +0530

    merge

---
+++ task 1.py
@@ -0,0 +1,7 @@
+n1=int(input("enter number 1:"))
+n2=int(input("enter number 2:"))
+n3=int(input("enter number 3:"))
+ans=n1+n2
+print("addition:",ans)
+average=ans/3
+print("average:",average)       
