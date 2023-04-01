94a694bdb5defd34ad7472540024b330162dc2bf
unknown <kishu7892@gmail.com>
2023-04-01 13:08:19 +0530

    merge other

---
+++ Daily task/29.03.23/task1.py
@@ -0,0 +1,11 @@
+# take two numbers and make a swap using third variable
+a=int(input("Enter the value of a :"))
+b=int(input("Enter the vaue of b :"))
+print("Value of a before swaping :",a)
+print("Value of b before swaping :",b)
+c=a
+a=b
+b=c
+print("Value of a after swaping :",a)
+print("Value of b after swaping :",b)
+      
