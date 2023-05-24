97d510f0be4866ed68224198d6ac3e790dda87f5
unknown <kishu7892@gmail.com>
2023-05-24 14:40:23 +0530

    merge other

--- Assignment/Assignment2&3/11.py
+++
@@ -1,8 +0,0 @@
-def unzip_list_of_tuples(list1):
-    unzipped_lists = list(zip(list1))
-    return unzipped_lists
-
-
-inp = [(1, 'a'), (2, 'b'), (3, 'c')]
-output_lists = unzip_list_of_tuples(inp)
-print(output_lists)
